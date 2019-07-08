efe36ed6f527072f12161a0c7bbfdb7c198c0245
miaoqs <quansheng.miao@fansunion.com>
2019-07-08 17:53:51 +0800

    Initial commit

---
+++ src/main/java/com/config/cloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.config.cloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 开启配置中心的服务端功能
+ * @EnableConfigServer
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
