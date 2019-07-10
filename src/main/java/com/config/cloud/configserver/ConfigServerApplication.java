0f7a1edf63ee4173a6d289c880e72c6d5f2bb180
miaoqs <quansheng.miao@fansunion.com>
2019-07-10 09:43:29 +0800

    测试的账号和密码

--- src/main/java/com/config/cloud/configserver/ConfigServerApplication.java
+++ src/main/java/com/config/cloud/configserver/ConfigServerApplication.java
@@ -2,12 +2,15 @@ package com.config.cloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * 开启配置中心的服务端功能
  * @EnableConfigServer
  */
+
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
