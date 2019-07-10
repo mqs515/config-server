332665d67a392f290f56b126be67e6a1c6d5ce7c
miaoqs <quansheng.miao@fansunion.com>
2019-07-10 14:33:46 +0800

    配置文件

--- src/main/java/com/config/cloud/configserver/ConfigServerApplication.java
+++ src/main/java/com/config/cloud/configserver/ConfigServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
+ * TODO  Spring-cloud 服务中的 server 服务
  * 开启配置中心的服务端功能
  * @EnableConfigServer
  */
